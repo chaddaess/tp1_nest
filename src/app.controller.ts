56e3975d7bdd05402fcf891e23b8831e99c69590
Selma Bouabidi <selma.bouabidi@insat.ucar.tn>
2024-04-06 03:22:31 +0100

    merging main

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { User } from './decorators/user.decorator';
+import { UserEntity } from './users/entities/user.entity';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello(@User() user : UserEntity): string {
+    console.log(user);
     return this.appService.getHello();
   }
 }
